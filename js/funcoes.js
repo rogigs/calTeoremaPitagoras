4a56e3f8e60f1aff3b952084b1d5499a88b41d51
Igor Gomes <igor.soares@cromai.com>
2020-04-08 21:56:25 -0300

    primeira refatoracao

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,47 +1,25 @@
+function percorreClasses(cor) {
+    var numbers = document.getElementsByClassName("bg-button")
+
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].style.backgroundColor = cor
+    }
+}
+
 function mudaCor(cor) {
+
     if (cor == "vermelho") {
-        document.getElementById("0").style.backgroundColor = "#dc3545";
-        document.getElementById("1").style.backgroundColor = "#dc3545";
-        document.getElementById("2").style.backgroundColor = "#dc3545";
-        document.getElementById("3").style.backgroundColor = "#dc3545";
-        document.getElementById("4").style.backgroundColor = "#dc3545";
-        document.getElementById("5").style.backgroundColor = "#dc3545";
-        document.getElementById("6").style.backgroundColor = "#dc3545";
-        document.getElementById("7").style.backgroundColor = "#dc3545";
-        document.getElementById("8").style.backgroundColor = "#dc3545";
-        document.getElementById("9").style.backgroundColor = "#dc3545";
-        document.getElementById("ponto").style.backgroundColor = "#dc3545";
-        document.getElementById("ce").style.backgroundColor = "#dc3545";
+        percorreClasses("#dc3545")
         document.getElementById("apagado").innerHTML = "ca";
     }
-    else if (cor == "verde") {
-        document.getElementById("0").style.backgroundColor = "#28a745";
-        document.getElementById("1").style.backgroundColor = "#28a745";
-        document.getElementById("2").style.backgroundColor = "#28a745";
-        document.getElementById("3").style.backgroundColor = "#28a745";
-        document.getElementById("4").style.backgroundColor = "#28a745";
-        document.getElementById("5").style.backgroundColor = "#28a745";
-        document.getElementById("6").style.backgroundColor = "#28a745";
-        document.getElementById("7").style.backgroundColor = "#28a745";
-        document.getElementById("8").style.backgroundColor = "#28a745";
-        document.getElementById("9").style.backgroundColor = "#28a745";
-        document.getElementById("ponto").style.backgroundColor = "#28a745";
-        document.getElementById("ce").style.backgroundColor = "#28a745";
+
+    if (cor == "verde") {
+        percorreClasses("#28a745")
         document.getElementById("apagado").innerHTML = "co";
     }
-    else if (cor == "azul") {
-        document.getElementById("0").style.backgroundColor = "#17a2b8";
-        document.getElementById("1").style.backgroundColor = "#17a2b8";
-        document.getElementById("2").style.backgroundColor = "#17a2b8";
-        document.getElementById("3").style.backgroundColor = "#17a2b8";
-        document.getElementById("4").style.backgroundColor = "#17a2b8";
-        document.getElementById("5").style.backgroundColor = "#17a2b8";
-        document.getElementById("6").style.backgroundColor = "#17a2b8";
-        document.getElementById("7").style.backgroundColor = "#17a2b8";
-        document.getElementById("8").style.backgroundColor = "#17a2b8";
-        document.getElementById("9").style.backgroundColor = "#17a2b8";
-        document.getElementById("ponto").style.backgroundColor = "#17a2b8";
-        document.getElementById("ce").style.backgroundColor = "#17a2b8";
+
+    if (cor == "azul") {
+        percorreClasses("#17a2b8")
         document.getElementById("apagado").innerHTML = "h";
     }
 
@@ -67,10 +45,10 @@ function numeros(num) {
     }
     else {
         window.alert("Escolha um lado(h, ca ou co)");
-    }    
+    }
 
     letras = indeOf(final, 2);
-    while("." != letras){
+    while ("." != letras) {
     }
 }
 
@@ -79,8 +57,7 @@ function numeroVar(num) {
 }
 
 function calcula() {
-    var numVar = document.getElementById("apagado2").innerText;
-
+  
     h = document.getElementById("mostraH").innerText;
     co = document.getElementById("mostraCo").innerText;
     ca = document.getElementById("mostraCa").innerText;
@@ -101,25 +78,25 @@ function calcula() {
     else if (ca == 0) {
         ca = h - co;
         raizCa = Math.sqrt(co);
-        if(ca <= 0){
+        if (ca <= 0) {
             document.getElementById("mostraCa").innerHTML = "Erro";
-        }else{
+        } else {
             document.getElementById("mostraCa").innerHTML = raizCa.toFixed(2);
         }
     }
     else if (co == 0) {
         co = h - ca;
         raizCo = Math.sqrt(co);
-        if(co <= 0){
+        if (co <= 0) {
             document.getElementById("mostraCo").innerHTML = "Erro";
-        }else{
+        } else {
             document.getElementById("mostraCo").innerHTML = raizCo.toFixed(2);
         }
     }
     else if (ca <= 0 || co <= 0 || h <= 0) {
         window.alert("Isto não é um triângulo pois um dos lado é 0 ou menor que 0");
     }
-    else{
+    else {
         window.alert("Permitido apenas duas variaveis");
         h = 0;
         ca = 0;
